9e9bca8f87bb71e48bf8f22c46ef63621d4a430e
Minh212 <minhhqgch190485@fpt.edu.vn>
2021-07-21 16:58:30 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const hostname = 'localhost'
+const port = 3000;
+
+const server = http.createServer((req, res)=>{
+    res.setHeader("Content-Type", "text/html");
+    switch(req.url){
+        case '/home':
+        case '/':
+            res.end('<h1>Home Page</h1>');
+            break;
+        case '/about':
+            res.end('<h1>About page');
+            break;
+        default:
+            res.end('<h1 style="color:red"> File not found!</h1>');
+            break;
+    }
+
+})
+
+server.listen(port,hostname, ()=>{
+    console.log('Server is running on Port' + port);
+})
